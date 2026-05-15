86fe57598a7779902b8cd9ae2bb92492ef013c03
Suzy-Pinheiro <pinheirosuzymelo@gmail.com>
2026-05-15 14:20:42 -0400

    Foi realizado refatoração no código

---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_IMC.Models
+{
+    public class Usuario
+    {
+        public string Name { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+    }
+}
